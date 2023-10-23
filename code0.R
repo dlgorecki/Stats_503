dd9513888a3e98fc9f8d188b2570a60dd77d95e5
dlgorecki <112571549+dlgorecki@users.noreply.github.com>
2023-10-23 13:48:41 -0400

    Add files via upload

---
+++ code0.R
@@ -0,0 +1,17 @@
+
+# Histogram
+hist(rnorm(1000))
+
+# Boxplot
+boxplot(rnorm(100))
+
+
+# Linear model
+
+lm(rnorm(100) ~ runif(100))
+
+# This is my first update- by prof
+
+#Hi, this is Drake!!! :)
+
+
